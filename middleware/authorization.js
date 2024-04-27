63c289e5574251e723651f8de2b90a6dc9818dca
kkopoku <kk.opoku@outlook.com>
2024-04-27 23:15:50 +0000

    added some ; and removed comments

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,21 +1,21 @@
-const { StatusCodes } = require('http-status-codes')
-const jwt = require('jsonwebtoken')
-const Organisation = require('../models/organisation')
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const Organisation = require('../models/organisation');
 
 const auth = async (req, res, next) => {
 
-    // validate authHeader
-    const authHeader = req.headers.authorization
+    const authHeader = req.headers.authorization;
+
     if (!authHeader || !authHeader.startsWith('Bearer')) {
-        res.status(StatusCodes.UNAUTHORIZED).json({ message: "Authentication Invalid" })
+        res.status(StatusCodes.UNAUTHORIZED).json({ message: "Authentication Invalid" });
+
     } else {
-        // receive token
+
         const token = authHeader.split(' ')[1]
 
-        // validate token
         try {
-            const payload = jwt.verify(token, process.env.JWT_SECRET)
-            const organisation = await Organisation.findOne({ _id: payload.organisation_id}).catch(error => { console.log(error) })
+            const payload = jwt.verify(token, process.env.JWT_SECRET);
+            const organisation = await Organisation.findOne({ _id: payload.organisation_id}).catch(error => { console.log(error) });
             req.employee = {
                 employee_id: payload.employee_id,
                 first_name: payload.first_name,
@@ -24,6 +24,7 @@ const auth = async (req, res, next) => {
                 organisation_name: organisation.organisation_name
             }
             next()
+
         } catch (error) {
             res.status(StatusCodes.UNAUTHORIZED).json({ message: "Authentication Invalid" })
         }
